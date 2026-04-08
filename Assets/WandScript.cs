a23ac710ac10a99d96a0c297a273a647b3ff58e7
leolaborie <leo-92i@hotmail.fr>
2026-04-08 14:02:54 +0200

    Wand sparkle and movement
    mouse controled

--- Assets/WandScript.cs
+++ Assets/WandScript.cs
@@ -16,19 +16,29 @@ public class TestScript : MonoBehaviour
     void Update()
     {
         transform.position = Vector3.Lerp(transform.position, Target, speed * Time.deltaTime);
+        Debug.Log(Target);
     }
+
 
     void OnGUI()
     {
-        range = range + Time.deltaTime;
+        Vector3 point = new Vector3();
+        Event   currentEvent = Event.current;
+        Vector2 mousePos = new Vector2();
 
-        if (range > 0.1f)
-        {
-            Event e = Event.current;
-            Debug.Log(e.mousePosition);
-            range = 0.0f;
-            Vector3 ToAdd = new Vector3(e.mousePosition.x * 0.01f, -e.mousePosition.y * 0.01f, 0);
-            Target = Origin + ToAdd;
-        }
+        // Get the mouse position from Event.
+        // Note that the y position from Event is inverted.
+        mousePos.x = currentEvent.mousePosition.x;
+        mousePos.y = Camera.main.pixelHeight - currentEvent.mousePosition.y;
+
+        point = Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, 10.0f));
+
+        // GUILayout.BeginArea(new Rect(20, 20, 250, 120));
+        // GUILayout.Label("Screen pixels: " + Camera.main.pixelWidth + ":" + Camera.main.pixelHeight);
+        // GUILayout.Label("Mouse position: " + mousePos);
+        // GUILayout.Label("World position: " + point.ToString("F3"));
+        // GUILayout.EndArea();
+
+        Target = point;
     }
 }
